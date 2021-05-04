d4c778764c505b6b1dc933cbe2f4fcf2253e7b7a
Shaher-11 <shahershamroukh@gmail.com>
2021-05-04 18:07:49 +0200

    Add events methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,15 +2,21 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
+    @events = Event.all
   end
 
   def show
+    @event = Event.find(params[:id])
   end
+
 
   def new
     @event = Event.new
   end
 
+def edit
+
+end 
   def create
     @event = current_user.created_events.build(event_params)
     if @event.save
@@ -20,9 +26,23 @@ class EventsController < ApplicationController
     end
   end
 
+  def destroy
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+  
   private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
 
   def event_params
     params.require(:event).permit(:title, :date, :location, :desc)
   end
 end
+
+
