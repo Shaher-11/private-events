5b4d0872f6ade9b40fccc74dc82215cb0ba2050e
Shaher-11 <shahershamroukh@gmail.com>
2021-05-05 10:03:11 +0200

    Change routes and show events page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
   resources :events
-  root to: 'home#index'
-  
+  root to: 'events#index'
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, only: [:show, :update, :destroy]
 end
