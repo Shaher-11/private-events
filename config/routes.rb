1661a9d2974bf05f6cdec6900fd2bed4990342aa
Shaher-11 <shahershamroukh@gmail.com>
2021-05-05 15:39:42 +0200

    Add routes for event_users model

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   root to: 'events#index'
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, only: [:show, :update, :destroy]
+
+  resources :event_users
+
+  resources :event_users do
+    match '/join', to: 'event_users#join', via: :post, on: :collection
+  end
+  
 end
 
 
